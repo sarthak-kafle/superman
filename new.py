ec1b834cda910e4e3a7c2cbbfcbd5b0057db38ac
sarthak kafle <sarthakkafle8@gmail.com>
2024-03-02 14:17:19 +0545

    practice

---
+++ new.py
@@ -0,0 +1,27 @@
+"""num1=int(input("enter a number for num 1"))
+num2=int(input("enter a number for num2"))
+sum=num1+num2
+print(sum)
+  
+light=input("enter light:")
+if(light=="red"):
+    print("stop")
+
+elif(light=="green"):
+    print("go")
+elif(light=="yellow"):
+    print("redy to stop")
+
+else:
+    ("light is broken")
+
+
+fruit=input("enter name")
+eat="yes" if fruit=="apple" else "no"
+print(eat)"""
+
+sweet=input("enter food")
+print("sweet" if sweet=="laddu" or sweet=="jalebi" else "not sweet")
+
+
+
